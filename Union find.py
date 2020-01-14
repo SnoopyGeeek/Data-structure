cdefb2d3cb7ddb012cb6974b929d948197f922f6
rui <cywang@sei.xjtu.edu.cn>
2020-01-14 17:06:00 +0800

    Update union find on graphs

--- Union find.py
+++ Union find.py
@@ -19,7 +19,7 @@ def unionfind(syn):
             ans[value].append(key)
         return list(ans.values())
 
-class UnionFind_graph:    #union find on a two diementional graph
+class UnionFind_graph:    #union find on a two diementional graph, can solve leetcode No.803
     def __init__(self,R,C):          #R and C represent the the quantity of rows and collumns, then R*C=totl number of nodes
         self.pair=[i for i in range(R*C+1)]
         self.sz=[1]*(R*C+1)
